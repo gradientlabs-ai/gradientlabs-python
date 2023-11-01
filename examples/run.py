105a388d545c2df01bc0020e48c82607fd3bc7ed
Daniel Upton <daniel@floppy.co>
2023-11-01 10:34:15 +0000

    First pass at our Python Client 🐍

---
+++ examples/run.py
@@ -0,0 +1,22 @@
+import os
+import uuid
+from gradient_labs import Client, ParticipantType
+from datetime import datetime
+
+client = Client(
+    api_key=os.environ["GLABS_API_KEY"],
+    base_url="http://localhost:4000",
+)
+
+conv = client.start_conversation(id=f"python-conversation-12345", customer_id="snake")
+
+client.add_message(
+    conversation_id=conv.id,
+    id="msg123",
+    body="Hello, world!",
+    participant_type=ParticipantType.CUSTOMER,
+    participant_id="user123",
+    created=datetime.now(),
+)
+
+client.cancel_conversation(id=conv.id)
